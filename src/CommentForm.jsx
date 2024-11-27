dee7152a271084b4480b5bf4d2e887a7819bd331
unknown <nouhailaelansari61@gmail.com>
2024-11-27 20:51:01 +0100

    creation composant CommentForm pour saisir des commentaires

---
+++ src/CommentForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function CommentForm() {
+  return (
+    <div>
+        <form >
+            <div >
+                <label htmlFor="author">Nom :</label>
+                <input
+                    type="text"
+                    id="author"
+                    name="author"
+                   
+                />
+            </div>
+            <div>
+                <label htmlFor="content">Commentaire :</label>
+                <textarea
+                    id="content"
+                    name="content"
+                    rows="3"
+                ></textarea>
+            </div>
+            <button type="submit">
+                Envoyer le commentaire
+            </button>
+
+        </form>
+    </div>
+  )
+}
+
+export default CommentForm
