948f5572366ba432b120286b705e47557b9e7dc4
SAAD-ELGHABA <saadlraba@gmail.com>
2024-11-28 13:25:32 +0100

    Étudiant 12 : Ajouter un bouton 'Réinitialiser' pour vider le formulaire.

---
+++ src/component/CommentForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+function CommentForm() {
+    const [commentaire,setCommentaire] = useState('')
+    const [nom,setNom] = useState('')
+    function handleRéinitialiser(){
+        setCommentaire('');
+        setNom('');
+    }
+  return (
+    <div>
+        <form >
+            <div >
+                <label htmlFor="author">Nom :</label>
+                <input
+                    type="text"
+                    id="author"
+                    name="author"
+                   value={nom}
+                   onChange={(event)=>setNom(event.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="content">Commentaire :</label>
+                <textarea
+                    id="content"
+                    name="content"
+                    rows="3"
+                    onChange={(event)=>setCommentaire(event.target.value)}
+                >
+                    {commentaire}
+                </textarea>
+            </div>
+            <button type="submit">
+                Envoyer le commentaire
+            </button>
+            <button onClick={handleRéinitialiser}>
+            "Réinitialiser"
+            </button>
+
+        </form>
+    </div>
+  )
+}
+
+export default CommentForm
